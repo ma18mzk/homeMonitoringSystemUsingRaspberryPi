7e1be7292b639d85d1a602ee0cf56061753d3f38
Zaheer <1706960@brunel.ac.uk>
2021-03-13 16:03:47 +0000

    The WebView that is intended to launch the camera is now functioning. A button has been created on the Options page for Camera 1, as a result of the WebView.

--- app/src/main/java/com/example/homemonitoringsystemusingraspberrypi/Camera1.java
+++ app/src/main/java/com/example/homemonitoringsystemusingraspberrypi/Camera1.java
@@ -2,14 +2,33 @@ package com.example.homemonitoringsystemusingraspberrypi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Camera1 extends AppCompatActivity {
+    private Button cam1Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera1);
         setTitle("Camera 1 Options");
+
+        cam1Btn = (Button) findViewById(R.id.viewCam1Btn);
+        cam1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLiveCam1();
+            }
+        });
+
     }
+
+    public void openLiveCam1() {
+        Intent intent = new Intent(this, Camera1Live.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/homemonitoringsystemusingraspberrypi/Camera1Live.java
@@ -0,0 +1,46 @@
+package com.example.homemonitoringsystemusingraspberrypi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Camera1Live extends AppCompatActivity {
+    private WebView camera1webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera1_live);
+        setTitle("Camera 1 Live Video Feed");
+
+        camera1webView = (WebView) findViewById(R.id.camera1WebView);
+        camera1webView.setWebViewClient(new WebViewClient());
+        camera1webView.loadUrl("http://www.google.com");
+
+        WebSettings webSettings = camera1webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setAllowFileAccess(true);
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (camera1webView.canGoBack()) {
+            camera1webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}

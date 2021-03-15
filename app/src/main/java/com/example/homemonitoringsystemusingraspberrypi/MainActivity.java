7a3b5556fad6cd9a926c94c00c8de033457ef384
Zaheer <1706960@brunel.ac.uk>
2021-03-15 22:03:58 +0000

    The help button on the first page is now functioning.

--- app/src/main/java/com/example/homemonitoringsystemusingraspberrypi/MainActivity.java
+++ app/src/main/java/com/example/homemonitoringsystemusingraspberrypi/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.homemonitoringsystemusingraspberrypi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button continueBtn;
+    private ImageButton helpBtn;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
                 openCameras();
             }
         });
+
+        helpBtn = (ImageButton) findViewById(R.id.mainHelpBtn);
+        builder = new AlertDialog.Builder(this);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setMessage("This is 1706960's FYP which uses the Raspberry Pi and a camera in order to create a surveillance style home monitoring system. To get started, please click on the \"CONTINUE\" button below.")
+                        .setCancelable(true);
+                AlertDialog alert = builder.create();
+                alert.setTitle("Help");
+                alert.show();
+
+            }
+        });
+
+
     }
 
     public void openCameras() {
